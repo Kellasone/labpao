9b5f73227b7db4241b9bf3cab0030358382e0454
Alexandru <a_mihai_lo@yahoo.ro>
2022-02-28 00:32:48 +0200

    prep done

---
+++ Lab3/src/pkg1/Main.java
@@ -0,0 +1,25 @@
+package pkg1;
+
+import pkg2.*;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("!");
+
+
+        Test test = new Test();
+        java.util.Date date = new java.util.Date();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.nextInt());
+        System.out.println(scanner.next());
+//        Arrays
+//        Arrays of arrays
+//        Classes
+    }
+}
